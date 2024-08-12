0dff327d6240d7872daf96a1367fdd6a18c4aa79
Destiny <zhengwy10112358@qq.com>
2024-08-12 11:53:34 +0800

    first_game

---
+++ settings.py
@@ -0,0 +1,19 @@
+class Settings:
+    def __init__(self) -> None:
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (100,140,200)
+        self.ship_speed = 1.5
+
+
+
+
+
+
+
+
+
+
+
+
+
